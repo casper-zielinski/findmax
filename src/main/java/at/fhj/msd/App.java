119a7ca40e747d3a4529bda9247ec714f841b564
Zielinski Casper <casper.zielinski.endercasper@gmail.com>
2025-05-02 23:31:27 +0200

    finished max Method with generic Type

--- src/main/java/at/fhj/msd/App.java
+++ src/main/java/at/fhj/msd/App.java
@@ -1,10 +1,33 @@
 package at.fhj.msd;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        String a = "aab";
+        String b = "aba";
+
+        int jeden = 1;
+        int dwa = 2;
+
+        System.out.println(max(a,b));
+        System.out.println(max(dwa, jeden));
+    }
+
+    public static <T extends Comparable<T>> T max(T a, T b){
+        if ((a.compareTo(b)) >= 1){
+            return a;
+        }
+        else if ((a.compareTo(b)) <= -1)
+        {
+            return b;
+        }
+        return null;
     }
 }
